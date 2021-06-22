3e26a45f843038f888497934fd3f789de4e8c830
Adranforth <85327786+Adranforth@users.noreply.github.com>
2021-06-22 14:39:51 +0800

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from app.controller import student, coursedesigner, lecturer, user
+
+# 定义注册蓝图方法
+def register_blueprints(app):
+    app.register_blueprint(student.studentBP,url_prefix='/student')
+    app.register_blueprint(coursedesigner.coursedesignerBP,url_prefix='/coursedesigner')
+    app.register_blueprint(lecturer.lecturerBP,url_prefix='/lecturer')
+    app.register_blueprint(user.userBP,url_prefix='/user')
+
+
+# 注册插件(数据库关联)
+def register_plugin(app):
+    from app.models.base import db
+    db.init_app(app)
+    from flask_migrate import Migrate
+    from app.models.course import Course, Cilo, Assessment, Grade
+    migrate = Migrate()
+    migrate.init_app(app, db)
+    # create_all要放到app上下文环境中使用
+    with app.app_context():
+        db.create_all()
+
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_object('app.config.setting') # 基本配置(setting.py) 
+    app.config.from_object('app.config.secure') # 重要参数配置(secure.py)
+    # 注册蓝图与app对象相关联
+    register_blueprints(app)
+    # 注册插件(数据库)与app对象相关联
+    register_plugin(app)
+    # 一定要记得返回app
+    return app
